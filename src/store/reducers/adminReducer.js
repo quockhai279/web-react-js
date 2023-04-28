cc099a5a08e7ab544ebf9b75b4f997c61c6cc298
Chau Quoc Khai <Khai.cq.27@gmail.com>
2023-04-28 13:30:27 +0700

    crud voi redux & gán default dropdown

--- src/store/reducers/adminReducer.js
+++ src/store/reducers/adminReducer.js
@@ -2,6 +2,7 @@ import actionTypes from '../actions/actionTypes';
 
 
 const initialState = {
+    isLoadingGender: false,
     genders: [],
     roles: [],
     positions: []
@@ -10,24 +11,42 @@ const initialState = {
 const adminReducer = (state = initialState, action) => {
     switch (action.type) {
         case actionTypes.FETCH_GENDER_START:
-            console.log('fetch gender star', action)
-            return {
-                ...state
-
-            }
-        case actionTypes.FETCH_GENDER_SUCCESS:
             let copyState = { ...state }
-            copyState.genders = action.data
-            console.log('fetch gender success', copyState)
+            copyState.isLoadingGender = true
             return {
                 ...copyState
-
             }
-        case actionTypes.FETCH_GENDER_FAILED:
-            console.log('fetch gender failed', action)
+        case actionTypes.FETCH_GENDER_SUCCESS:
+            state.genders = action.data
+            state.isLoadingGender = false
             return {
                 ...state
-
+            }
+        case actionTypes.FETCH_GENDER_FAILED:
+            state.isLoadingGender = false
+            state.genders = []
+            return {
+                ...state
+            }
+        case actionTypes.FETCH_POSITION_SUCCESS:
+            state.positions = action.data
+            return {
+                ...state
+            }
+        case actionTypes.FETCH_POSITION_FAILED:
+            state.positions = []
+            return {
+                ...state
+            }
+        case actionTypes.FETCH_ROLE_SUCCESS:
+            state.roles = action.data
+            return {
+                ...state
+            }
+        case actionTypes.FETCH_ROLE_FAILED:
+            state.roles = []
+            return {
+                ...state
             }
 
         default:
