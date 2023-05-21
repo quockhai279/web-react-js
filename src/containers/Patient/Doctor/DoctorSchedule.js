94d17c83e5fa3b282d4f83de173450ddc6aa8aa5
Chau Quoc Khai <Khai.cq.27@gmail.com>
2023-05-21 14:37:04 +0700

    video 74-75

--- src/containers/Patient/Doctor/DoctorSchedule.js
+++ src/containers/Patient/Doctor/DoctorSchedule.js
@@ -1,29 +1,85 @@
 import React, { Component } from 'react';
 import { connect } from "react-redux";
 import './DoctorSchedule.scss'
+import moment from 'moment';
+import localization from 'moment/locale/vi'
+import { getScheduleDoctorByDate } from '../../../services/userService';
+import { LANGUAGES } from '../../../utils';
 
 class DoctorSchedule extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
-
+            allDays: [],
         }
     }
 
-    async componentDidMount() {
-
+    componentDidMount() {
+        let { language } = this.props
+        this.setArrDays(language)
     }
 
-    async componentDidUpdate(prevProps, preState, snapshot) {
+    setArrDays = (language) => {
+        let allDays = [];
+        for (let i = 0; i < 7; i++) {
+            let object = {};
+            if (language === LANGUAGES.VI) {
+                object.label = moment(new Date()).add(i, 'days').format('dddd - DD/MM');
+            } else {
+                object.label = moment(new Date()).add(i, 'days').locale('en').format('ddd - DD/MM');
+            }
+            object.value = moment(new Date()).add(i, 'days').startOf('day').valueOf();
+            allDays.push(object)
+        }
 
+        this.setState({
+            allDays: allDays
+        })
+    }
+
+    componentDidUpdate(prevProps, preState, snapshot) {
+        if (this.props.language !== prevProps.language) {
+            this.setArrDays(this.props.language)
+        }
     }
 
 
+    handleOnChangeSelect = async (event) => {
+        if (this.props.doctorIdFromParent && this.props.doctorIdFromParent !== -1) {
+            let doctorId = this.props.doctorIdFromParent;
+            let date = event.target.value
+            let res = await getScheduleDoctorByDate(doctorId, date)
+            console.log('check res', res);
+        }
+    }
 
     render() {
+        let { allDays } = this.state
+
         return (
-            <div className=''>asdf</div>
+            <div className='doctor-schedule-container'>
+                <div className='all-schedule'>
+                    <select onChange={(event) => this.handleOnChangeSelect(event)}>
+                        {allDays && allDays.length > 0 &&
+                            allDays.map((item, index) => {
+                                return (
+                                    <option
+                                        value={item.value}
+                                        key={index}
+                                    >
+                                        {item.label}
+                                    </option>
+
+                                )
+                            })}
+
+                    </select>
+                </div>
+                <div className='all-available-time'>
+
+                </div>
+            </div>
         );
     }
 }
